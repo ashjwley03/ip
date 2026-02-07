eb649808c660e6361cc0ae00c2bbe21afb6fece8
ashley <ashleyjwilliams03@gmail.com>
2026-02-07 18:42:30 +0800

    Add date parsing for deadlines

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,39 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate byDate;
+    protected String byString;
+    protected boolean hasDate;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        try {
+            this.byDate = LocalDate.parse(by);
+            this.hasDate = true;
+            this.byString = by;
+        } catch (DateTimeParseException e) {
+            this.byString = by;
+            this.hasDate = false;
+        }
+    }
+
+    public String getByForStorage() {
+        if (hasDate) {
+            return byDate.toString();
+        }
+        return byString;
     }
 
     @Override
     public String toString() {
-        return "[D][" + getStatusIcon() + "] " + description + " (by: " + by + ")";
+        String byDisplay;
+        if (hasDate) {
+            byDisplay = byDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        } else {
+            byDisplay = byString;
+        }
+        return "[D][" + getStatusIcon() + "] " + description + " (by: " + byDisplay + ")";
     }
 }
